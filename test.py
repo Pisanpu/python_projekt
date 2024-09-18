e5b23ad442614ba7931497700ef57e563a40d7e7
messium <danielholstrom@gmail.com>
2024-09-18 15:15:06 +0200

    Minerva kolla här jag har laggt till en cool funktion

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
 print("Hello world")
 
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+
+
+
+
+
+
+
+
